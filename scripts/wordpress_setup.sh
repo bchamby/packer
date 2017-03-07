bf6a8b2d7e4061d2ef6905c82652459c9fcdf5a2
Brad Chamberlain <chamby@gmail.com>
2017-03-07 12:40:51 -0500

    put mysql passwd creation at the end

--- scripts/wordpress_setup.sh
+++ scripts/wordpress_setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 sudo wget https://wordpress.org/latest.tar.gz -O /var/tmp/wordpress_latest.tar.gz
-sudo tar -xzf /var/tmp/wordpress_latest.tar.gz
+sudo mkdir /var/tmp/wordpress
+sudo tar -xzf /var/tmp/wordpress_latest.tar.gz -C /var/tmp
 sudo mysql -sfu root < "/var/tmp/wordpress_database.sql"
 sudo mkdir /var/www/html/blog
-sudo mv /var/tmp/wordpress/* /var/www/html/blog
+sudo cp -R /var/tmp/wordpress/* /var/www/html/blog
+sudo cp /var/tmp/wp-config.php /var/www/html/blog
 sudo usermod -a -G www apache
 sudo chown -R apache /var/www
 sudo chgrp -R www /var/www
