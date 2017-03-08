fc79730b0ee542c224b352e3184b131affe39f11
Brad Chamberlain <chamby@gmail.com>
2017-03-08 06:19:40 -0500

    broke out MySQL password to secrets file

--- scripts/wordpress_database.sql
+++ scripts/wordpress_database.sql
@@ -1,5 +1,4 @@
 CREATE USER 'wordpress-user'@'localhost' IDENTIFIED BY 'passw0rd!';
 CREATE DATABASE `wordpress-db`;
 GRANT ALL PRIVILEGES ON `wordpress-db`.* TO "wordpress-user"@"localhost";
-UPDATE mysql.user SET Password=PASSWORD('passw0rd!') WHERE User='root';
 FLUSH PRIVILEGES;
