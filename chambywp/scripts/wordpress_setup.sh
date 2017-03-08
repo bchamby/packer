31b31544bffc82d51485203f10e3b317ef6f96de
Brad Chamberlain <chamby@gmail.com>
2017-03-08 15:37:57 -0500

    changing blog path to /var/www/html

--- chambywp/scripts/wordpress_setup.sh
+++ chambywp/scripts/wordpress_setup.sh
@@ -5,8 +5,8 @@ sudo mkdir /var/tmp/wordpress
 sudo tar -xzf /var/tmp/wordpress_latest.tar.gz -C /var/tmp
 sudo mysql -sfu root < "/var/tmp/wordpress_database.sql"
 sudo mkdir /var/www/html/blog
-sudo cp -R /var/tmp/wordpress/* /var/www/html/blog
-sudo cp /var/tmp/wp-config.php /var/www/html/blog
+sudo cp -R /var/tmp/wordpress/* /var/www/html
+sudo cp /var/tmp/wp-config.php /var/www/html
 sudo usermod -a -G www apache
 sudo chown -R apache /var/www
 sudo chgrp -R www /var/www
