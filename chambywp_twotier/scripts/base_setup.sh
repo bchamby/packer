14274ef11a17cdacc9072106bbeda7e0270afee6
Brad Chamberlain <chamby@gmail.com>
2017-03-09 08:54:23 -0500

    working on separate RDS instance

---
+++ chambywp_twotier/scripts/base_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sleep 30
+
+# yum update
+sudo yum update -y
+
+# Install dependencies
+sudo yum install -y httpd24 php70 php70-mysqlnd
+
+# Apache config
+sudo service httpd start
+sudo chkconfig httpd on
+sudo groupadd www
+sudo usermod -a -G www ec2-user
---
+++ chambywp_twotier/scripts/wordpress_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo wget https://wordpress.org/latest.tar.gz -O /var/tmp/wordpress_latest.tar.gz
+sudo mkdir /var/tmp/wordpress
+sudo tar -xzf /var/tmp/wordpress_latest.tar.gz -C /var/tmp
+sudo mkdir /var/www/html/blog
+sudo cp -R /var/tmp/wordpress/* /var/www/html
+sudo cp /var/tmp/wp-config.php /var/www/html
+sudo usermod -a -G www apache
+sudo chown -R apache /var/www
+sudo chgrp -R www /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+sudo service httpd restart
+sudo chkconfig httpd on
